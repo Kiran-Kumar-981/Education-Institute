658ba3a3ad4e21fed06e98e25370a90bbedaca80
Mr. KK <163104299+Kiran-Kumar-981@users.noreply.github.com>
2024-06-20 21:26:11 +0530

    Update main.go

--- main.go
+++ main.go
@@ -1,13 +1,14 @@
+//gin-framework or gin package give us the used to reduce lines of code and helps in scaling the application and helps in easy routing of application
 package main
 
 import (
 	"database/sql"
 	"fmt"
-	"institute/mysql"
+	"institute/mysql"  //hot code is secured in different package to increase security and reduce boiler plate
 	"net/http"
 
-	"github.com/gin-gonic/gin"
-	_ "github.com/go-sql-driver/mysql"
+	"github.com/gin-gonic/gin" //third-party package 
+	_ "github.com/go-sql-driver/mysql" //mysql driver
 )
 
 var (
@@ -38,7 +39,7 @@ type EnqueryData struct {
 	PhNumber      string `form:"PhNumber" binding:"required,len=10"`
 	Course        string `form:"Course" binding:"required"`
 }
-
+//initialising the db is good practice as it need's to store and retrive the information from database as per the requirment
 func init() {
 	dataBase, err = sql.Open("mysql", mysql.DataBaseURL)
 	if err != nil {
@@ -56,13 +57,15 @@ func init() {
 func main() {
 	defer dataBase.Close()
 	router := gin.Default()
+	//static files are given in static folder which are  css files
 	router.Static("/static", "./static")
+	//HTML files are stored in templet folder for the readability and modularity
 	router.LoadHTMLGlob("templete/*.html")
-
+	//localhost will show the login page as index page for this
 	router.GET("/", func(c *gin.Context) {
 		c.HTML(http.StatusOK, "login.html", nil)
 	})
-
+	//grouping and authorizing will help in use friendly and secure from outsiders
 	authorized := router.Group("/")
 	authorized.Use(authRequired())
 	{
@@ -92,11 +95,11 @@ func main() {
 func home(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "home.html", nil)
 }
-
+//opens the admission form folder to get the use input
 func admissionForm(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "admissionForm.html", nil)
 }
-
+//viewStudent function gets the details of students from the daytabase and displays in the table fomate
 func viewStudent(ctx *gin.Context) {
 	students, err := fetchStudentData()
 	if err != nil {
@@ -105,21 +108,23 @@ func viewStudent(ctx *gin.Context) {
 	}
 	ctx.HTML(http.StatusOK, "viewStudents.html", gin.H{"students": students})
 }
-
+//after succesfully inserting the details of new student it displays succesfull insertion for admission
 func detailsSubmited(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "inserted.html", nil)
 }
+//after succesfully inserting the details of new student it displays succesfull insertion for enquery
 func detailsEnqSubmited(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "DetailsEnqSubmited.html", nil)
 }
+//student can pay fee directly in viewstudents portal onclick redirects to payment portal
 func payFee(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "PayFee.html", nil)
 }
-
+//gives the faculty to store the details of the students for enquery about the courese present and they want
 func enqueryForm(ctx *gin.Context) {
 	ctx.HTML(http.StatusOK, "enqueryForm.html", nil)
 }
-
+//admissions gets the details from the students and inserts to the database
 func admissions(ctx *gin.Context) {
 	var userData UserData
 	if err := ctx.ShouldBind(&userData); err != nil {
@@ -143,7 +148,7 @@ func admissions(ctx *gin.Context) {
 
 	ctx.HTML(http.StatusOK, "inserted.html", nil)
 }
-
+//this is the login form that is authentication and sets the cookie in browser for further purposes
 func login(ctx *gin.Context) {
 	username := ctx.PostForm("username")
 	password := ctx.PostForm("password")
@@ -156,7 +161,7 @@ func login(ctx *gin.Context) {
 		})
 	}
 }
-
+//authRequired handles the cookie to be set or not 
 func authRequired() gin.HandlerFunc {
 	return func(ctx *gin.Context) {
 		authenticated, err := ctx.Cookie("authenticated")
@@ -168,12 +173,12 @@ func authRequired() gin.HandlerFunc {
 		ctx.Next()
 	}
 }
-
+//logout removes the cookie and brings to the login page
 func logout(ctx *gin.Context) {
 	ctx.SetCookie("authenticated", "", -1, "/", "", false, true)
 	ctx.Redirect(http.StatusSeeOther, "/")
 }
-
+//enquery gets the detaies of the students that are intrested and willing to join the institute
 func enquery(ctx *gin.Context) {
 	var enqueryData EnqueryData
 	if err := ctx.ShouldBind(&enqueryData); err != nil {
@@ -199,7 +204,7 @@ func enquery(ctx *gin.Context) {
 
 	ctx.HTML(http.StatusOK, "inserted.html", nil)
 }
-
+//enqueryViewStudent give acceses to faculty to contact the students visited and still need to join 
 func enqueryViewStudent(ctx *gin.Context) {
 	enqStudents, err := fetchEnqStudentData()
 	if err != nil {
@@ -208,7 +213,7 @@ func enqueryViewStudent(ctx *gin.Context) {
 	}
 	ctx.HTML(http.StatusOK, "enqueryviewstudents.html", gin.H{"students": enqStudents})
 }
-
+//fetchStudentData featches the raw data from database and send to the viewStudents table
 func fetchStudentData() ([]UserData, error) {
 	rows, err := dataBase.Query("SELECT * FROM students")
 	if err != nil {
@@ -226,7 +231,7 @@ func fetchStudentData() ([]UserData, error) {
 	}
 	return students, nil
 }
-
+//fetchEnqStudentData featches the raw data from database and send to the viewEnqStudents table
 func fetchEnqStudentData() ([]EnqueryData, error) {
 	rows, err := dataBase.Query("SELECT * FROM enqueries")
 	if err != nil {
